f849bb80a1c0281af5b0bd774d3a9bdf4934eb0d
Alexandr Savchenko <miklester7@gmail.com>
2021-11-17 13:43:21 +0900

    HW_4(2)

--- HW_4/HW_4.cpp
+++ HW_4/HW_4.cpp
@@ -1,25 +1,9 @@
 
 #include <iostream>
 
-int main()
+int math(int Par, int N)
 {
-    int N, Parity;
-
-    std::cout << "Set the value to N" << '\n';
-    std::cin >> N;
-
-    //Вывод чётных значений
-    for (int i = 0; i < N; i+=2)
-    {
-        std::cout << i <<'\n';
-    }
-
-    std::cout << "Enter the number" << '\n';
-    std::cout << "Even numbers 1 " << '\n' << "odd numbers 0 "<<'\n';
-    std::cin >> Parity;
-
-    //Вывод чётных или нечётных значение
-    switch (Parity)
+    switch (Par)
     {
     case 1:
         for (int i = 0; i < N; i += 2)
@@ -30,11 +14,32 @@ int main()
     case 0:
         for (int i = 0; i < N; ++i)
         {
-            if(i % 2 == 1)
+            if (i % 2 == 1)
                 std::cout << i << '\n';
         }
         break;
     default:
         std::cout << "incorrect value" << '\n';
     }
+    return 0;
+}
+
+
+int main()
+{
+    int N, Parity;
+
+    std::cout << "Set the value to N" << '\n';
+    std::cin >> N;
+
+    //Вывод чётных значений
+    for (int i = 0; i < N; i += 2)
+    {
+        std::cout << i << '\n';
+    }
+
+    std::cout << "Enter the number" << '\n';
+    std::cout << "Even numbers 1 " << '\n' << "odd numbers 0 " << '\n';
+    std::cin >> Parity;
+    math (Parity,N);
 }
