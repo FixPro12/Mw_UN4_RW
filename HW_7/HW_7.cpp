3047b103b645635732052e89d0e54b6a8218f28e
Alexandr Savchenko <miklester7@gmail.com>
2021-12-01 17:43:31 +0900

    HW_7(2)

--- HW_7/HW_7.cpp
+++ HW_7/HW_7.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <stack>  
 
+class Stack
+{
+private:
+    std::stack <int> steck;
+    int i = 0;
+    int a;
+public:
+    void GetStack()
+    {
+        std::cout << "Enter six numbers: " << std::endl;
+
+        while (i != 6) {
+            std::cin >> a;
+
+            steck.push(a);  // добавляем введенные числа
+            i++;
+        }
+        std::cout << "Top of stack : " << steck.top() << std::endl; // выводим верхний элемент
+
+        steck.pop();  // удаляем верхний элемент
+        std::cout << "Top element removed " << std::endl;
+
+        std::cout << "Top of stack : " << steck.top() << std::endl; // выводим верхний элемент
+     }
+
+};
 
 int main() {
-
-    std::stack <int> steck;  
-
-    int i = 0;
-
-    std::cout << "Enter six numbers: " << std::endl; 
-
-    while (i != 6) {
-        int a;
-        std::cin >> a;
-
-        steck.push(a);  // добавляем введенные числа
-        i++;
-    }
-
-    std::cout << "Top of stack : " << steck.top() << std::endl; // выводим верхний элемент
-
-    steck.pop();  // удаляем верхний элемент
-    std::cout<<"Top element removed "<< std::endl;
-
-    std::cout << "Top of stack : " << steck.top() << std::endl; // выводим верхний элемент
+    Stack p;
+    p.GetStack();
 }
