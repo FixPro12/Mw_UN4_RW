0a441633c7ff576fa03ae9cb38baf7fa68a6111a
Alexandr Savchenko <miklester7@gmail.com>
2021-12-08 14:34:52 +0900

    HW_7(3)

---
+++ HW_7(358)/HW_7(358).cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+using namespace std;
+template <typename T>
+class Stack
+{
+private:
+	T element = 0;
+	int size = 0;
+	T* arr;
+public:
+	Stack()
+	{
+		T* arr = new T[size];
+		cout << "enter the size of the array " << endl;
+		cin >> size;
+		cout << "Enter the elements " << endl;
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> element;
+			arr[i] = element;
+		}
+	}
+	~Stack()
+	{
+		delete[] arr;
+	}
+};
+
+int main()
+{
+	Stack<int> arr;
+	return 0;
+}
+
