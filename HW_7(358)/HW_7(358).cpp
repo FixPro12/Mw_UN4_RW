0da6e760dabfc9115273968009419c0f54660647
Alexandr Savchenko <miklester7@gmail.com>
2021-12-12 12:38:05 +0900

    HW_7(6)

--- HW_7(358)/HW_7(358).cpp
+++ HW_7(358)/HW_7(358).cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 
 using namespace std;
@@ -10,7 +9,7 @@ class Stack
 private:
 
 	T element = 0;
-	int size, i;
+	int size, i=0;
 	T* arr;
 
 public:
@@ -19,15 +18,25 @@ public:
 	{
 		cout << "enter the size of the array " << endl;
 		cin >> size;
+		if (size == 0)
+		{
+			cout << "Error:You entered zero value!" << endl;
+			exit(0);
+		}
 		arr = new T[size];
 	}
 	void push()
 	{
-		cout << "Enter the elements " << endl;
-		for (i = 0; i < size; i++)
+		if (i < size)
 		{
+			cout << "Enter the elements " << endl;
 			cin >> element;
 			arr[i] = element;
+			i++;
+		}
+		else
+		{
+			cout << "Array size exceeded!"<<endl;
 		}
 	}
 	void pop()
@@ -46,6 +55,8 @@ public:
 int main()
 {
 	Stack<int> arr;
+	arr.push();
+	arr.push();
 	arr.push();
 	arr.pop();
 	arr.pop();
